be05bcb33bc4aff3bdd5e076db7d35c55b1aee5e
Skuldo <matteo.milin@epitech.eu>
2026-01-12 13:16:10 +0100

    test(all): add ConfigService mocks for environment variables in unit tests

--- src/github/github.controller.spec.ts
+++ src/github/github.controller.spec.ts
@@ -78,6 +78,25 @@ describe('GithubController', () => {
             }),
           },
         },
+        {
+          provide: ConfigService,
+          useValue: {
+            get: jest.fn((key: string) => {
+              const config = {
+                GITHUB_CLIENT_ID: 'test-github-id',
+                GITHUB_CLIENT_SECRET: 'test-github-secret',
+              };
+              return config[key];
+            }),
+            getOrThrow: jest.fn((key: string) => {
+              const config = {
+                GITHUB_CLIENT_ID: 'test-github-id',
+                GITHUB_CLIENT_SECRET: 'test-github-secret',
+              };
+              return config[key];
+            }),
+          },
+        },
       ],
     }).compile();
 
--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Hook } from '../shared/entities/hook.entity';
+import { Provider } from '../shared/entities/provider.entity';
 import { User } from '../shared/entities/user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
@@ -26,6 +27,16 @@ describe('UsersController', () => {
         {
           provide: UsersService,
           useValue: mockUsersService,
+        },
+        {
+          provide: getRepositoryToken(Provider),
+          useValue: {
+            find: jest.fn(),
+            findOne: jest.fn(),
+            create: jest.fn(),
+            save: jest.fn(),
+            remove: jest.fn(),
+          },
         },
       ],
     }).compile();
