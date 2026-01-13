831f161087006b4415fff1ebfc09aa44b7c2b878
mambokara <cbergaent@gmail.com>
2026-01-13 19:33:02 +0100

    fix(tests): reorder import statements in users.controller.spec.ts for consistency

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,8 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Provider } from 'src/shared/entities/provider.entity';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
